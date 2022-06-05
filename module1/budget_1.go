ab5bb2247d909f9d9c940d1169d199831ae38bba
Mohamed Refaie <MohamedRefaie@rightpoint.com>
2022-06-05 15:46:16 -0500

    structs v4

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,14 +1,12 @@
 package module1
 
 // Budget stores budget information
-
 type budget struct {
 	Max   float32
 	Items []Item
 }
 
 // Item stores item information
-
 type Item struct {
 	Description string
 	Price       float32
