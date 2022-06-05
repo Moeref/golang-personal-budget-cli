7d5f9a901b2ae87d2864b96ccb348f78c3cd0323
Mohamed Refaie <MohamedRefaie@rightpoint.com>
2022-06-05 15:37:18 -0500

    added structs v2

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -3,13 +3,13 @@ package module1
 // Budget stores budget information
 
 type budget struct {
-	Max   float32
-	Items []Item
+	var Max   float32
+	var Items []Item
 }
 
 // Item stores item information
 
 type Item struct {
-	Description string
-	Price       float32
+	var Description string
+	var Price       float32
 }
